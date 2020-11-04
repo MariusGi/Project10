6f5b662606335fd4a679bb5d345b0c9d276218f0
MariusGi <mariusgirnis@gmail.com>
2020-11-04 21:18:19 +0200

    add initial dependancies and public holidays controller

---
+++ app/src/Controller/PublicHolidaysController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PublicHolidaysController extends AbstractController
+{
+    /**
+     * @Route("/public/holidays", name="public_holidays")
+     */
+    public function index(): Response
+    {
+        return $this->render('public_holidays/index.html.twig', [
+            'controller_name' => 'PublicHolidaysController',
+        ]);
+    }
+}
